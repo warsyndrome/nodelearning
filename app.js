c898abf99088e25771798cee2d42a4cc5d3c9853
Aman Chaturvedi <somyachaturvedi1964@gmail.com>
2024-02-18 11:56:38 +0530

    npm sharecode nodemon installation and uninstall package json

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/'){
+    if (req.url === '/') {
         res.end('welcomw to our home page')
     }
-    if(req.url === '/about'){
+    if (req.url === '/about') {
         res.end('here is our about page.')
     }
     res.end(`
     <h1>OOPs</h1>
     <a href="/"> back home</a>
-    `)
+    `);
 });
 
-server.listen(8000);
+server.listen(8000); 
+
+
+//2:26:40
